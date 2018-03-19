ec5c868d730080df46e0c11fa771feb0508c6dcb
Zhaoxing Wu <wuzhaoxing@hotmail.com>
2018-03-19 17:59:00 -0400

    Added first working prototype version of game

---
+++ game/images/tiles.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="tiles" tilewidth="64" tileheight="64" tilecount="3" columns="3">
+ <image source="tiles.png" width="192" height="64"/>
+</tileset>
