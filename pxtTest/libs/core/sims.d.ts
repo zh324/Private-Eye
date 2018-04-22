afe3b67a8740416321483b48a416512236e3e19e
Zhaoxing Wu <wuzhaoxing@hotmail.com>
2018-04-21 22:19:49 -0400

    Modified most of simulator.ts, added separation of game logic and graphics, implemented action log and tween chaining. Game can now run in independently in the background with graphics queued. Changed the API to only have face directions and move forward commands.

--- pxtTest/libs/core/sims.d.ts
+++ pxtTest/libs/core/sims.d.ts
@@ -1,97 +1,39 @@
 // Auto-generated from simulator. Do not edit.
 declare namespace Robot {
     /**
-     * Sets the velocity of the player
-     * @param dimension x or y
-     * @param value new velocity
+     * Makes the robot face up.
      */
-    //% blockId=phasersetvelocity block="set player velocity %dimension to %value"
-    //% shim=Robot::setVelocity
-    function setVelocity(dimension: Dimension, value: number): void;
-
-    /** 
-     * Checks if the player is hitting the platforms
-     */
-    //% blockId=phaserhitplatform block="is player hitting platforms"
-    /*export function isHittingPlatforms(): boolean {
-    const b = board();
-    const hitPlatform = b.game.physics.arcade.collide(b.robot, b.platforms);
-    return hitPlatform;
-    }*/
-    /**
-     * Plays an animation on the player
-     * @param animation 
-     */
-    //% blockId=phaserplayanimation block="player play animation %animation"
-    //% shim=Robot::playAnimation
-    function playAnimation(animation: Animation): void;
+    //% blockId=faceUp block="face up"
+    //% shim=Robot::faceUpAsync promise
+    function faceUp(): void;
 
     /**
-     * Move the robot forward
+     * Makes the robot face down.
      */
-    //% blockId=moveUp block="moveUp"
-    //% shim=Robot::moveupAsync promise
-    function moveup(): void;
+    //% blockId=faceDown block="face down"
+    //% shim=Robot::faceDownAsync promise
+    function faceDown(): void;
 
     /**
-     * Move the robot forward
+     * Makes the robot face left.
      */
-    //% blockId=moveDown block="moveDown"
-    //% shim=Robot::movedownAsync promise
-    function movedown(): void;
+    //% blockId=faceLeft block="face left"
+    //% shim=Robot::faceLeftAsync promise
+    function faceLeft(): void;
 
     /**
-     * Move the robot forward
+     * Makes the robot face right.
      */
-    //% blockId=moveLeft block="moveLeft"
-    //% shim=Robot::moveleftAsync promise
-    function moveleft(): void;
+    //% blockId=faceRight block="face right"
+    //% shim=Robot::faceRightAsync promise
+    function faceRight(): void;
 
     /**
-     * Move the robot forward
+     * Moves the robot 1 step in the direction it is facing.
      */
-    //% blockId=moveRight block="moveRight"
-    //% shim=Robot::moverightAsync promise
-    function moveright(): void;
-
-    /**
-     * Moves the sprite forward
-     * @param direction the direction to turn, eg: Direction.Left
-     * @param angle degrees to turn, eg:90
-     */
-    //% weight=85
-    //% blockId=sampleTurn block="turn %direction|by %angle degrees"
-    //% angle.min=-180 angle.max=180
-    //% shim=Robot::turnAsync promise
-    function turn(direction: Direction, angle: number): void;
-
-    /**
-     * Triggers when the robot bumps a wall
-     * @param handler 
-     */
-    //% blockId=onBump block="on bump"
-    //% shim=Robot::onBump
-    function onBump(handler: () => void): void;
-
-}
-declare namespace loops {
-    /**
-     * Repeats the code forever in the background. On each iteration, allows other code to run.
-     * @param body the code to repeat
-     */
-    //% help=functions/forever weight=55 blockGap=8
-    //% blockId=device_forever block="forever"
-    //% shim=loops::forever
-    function forever(body: () => void): void;
-
-    /**
-     * Pause for the specified time in milliseconds
-     * @param ms how long to pause for, eg: 100, 200, 500, 1000, 2000
-     */
-    //% help=functions/pause weight=54
-    //% block="pause (ms) %pause" blockId=device_pause
-    //% shim=loops::pauseAsync promise
-    function pause(ms: number): void;
+    //% blockId=moveForward block="walk forward"
+    //% shim=Robot::moveForwardAsync promise
+    function moveForward(): void;
 
 }
 
--- pxtTest/sim/api.ts
+++ pxtTest/sim/api.ts
@@ -3,134 +3,80 @@
 /// <reference path="../node_modules/phaser-ce/typescript/phaser.d.ts" />
 
 namespace pxsim.Robot {
-    /**
-     * Sets the velocity of the player
-     * @param dimension x or y
-     * @param value new velocity
-     */
-    //% blockId=phasersetvelocity block="set player velocity %dimension to %value"
-    export function setVelocity(dimension: Dimension, value: number) {
-        //  Reset the players velocity (movement)
-        switch (dimension) {
-            case Dimension.X:
-                board().robot.body.velocity.x = value;
-                break;
-            case Dimension.Y:
-                board().robot.body.velocity.y = value;
-                break;
-        }
-    }
-
-    /** 
-     * Checks if the player is hitting the platforms
-    */
-   //% blockId=phaserhitplatform block="is player hitting platforms"
-    /*export function isHittingPlatforms(): boolean {
-        const b = board();
-        const hitPlatform = b.game.physics.arcade.collide(b.robot, b.platforms);
-        return hitPlatform;
-    }*/
 
     /**
-     * Plays an animation on the player
-     * @param animation 
+     * Makes the robot face up.
      */
-    //% blockId=phaserplayanimation block="player play animation %animation"
-    export function playAnimation(animation: Animation) {
-        const b = board();
-        switch(animation) {
-            case Animation.Left: b.robot.animations.play('left'); break;
-            case Animation.Right: b.robot.animations.play('right'); break;
-        }
-    }
-
-    /**
-     * Move the robot forward
-     */
-    //% blockId=moveUp block="moveUp"
-    export function moveupAsync() {
+    //% blockId=faceUp block="face up"
+    export function faceUpAsync() {
         // Move the robot forward
-        board().moveUp();
-        return Promise.delay(1000)
+        board().faceUp();
+        board().printRobot();
+        return Promise.delay(100)
     }
 
     /**
-     * Move the robot forward
+     * Makes the robot face down.
      */
-    //% blockId=moveDown block="moveDown"
-    export function movedownAsync() {
-        board().moveDown();
-        return Promise.delay(1000)
+    //% blockId=faceDown block="face down"
+    export function faceDownAsync() {
+        board().faceDown();
+        board().printRobot();
+        return Promise.delay(100)
     }
 
     /**
-     * Move the robot forward
+     * Makes the robot face left.
      */
-    //% blockId=moveLeft block="moveLeft"
-    export function moveleftAsync() {
-        board().moveLeft();
-        return Promise.delay(1000)
+    //% blockId=faceLeft block="face left"
+    export function faceLeftAsync() {
+        board().faceLeft();
+        board().printRobot();
+        return Promise.delay(100)
     }
 
     /**
-     * Move the robot forward
+     * Makes the robot face right.
      */
-    //% blockId=moveRight block="moveRight"
-    export function moverightAsync() {
-        board().moveRight();
-        return Promise.delay(1000)
+    //% blockId=faceRight block="face right"
+    export function faceRightAsync() {
+        board().faceRight();
+        board().printRobot();
+        return Promise.delay(100)
     }
 
-    /**
-     * Moves the sprite forward
-     * @param direction the direction to turn, eg: Direction.Left
-     * @param angle degrees to turn, eg:90
+     /**
+     * Moves the robot 1 step in the direction it is facing.
      */
-    //% weight=85
-    //% blockId=sampleTurn block="turn %direction|by %angle degrees"
-    //% angle.min=-180 angle.max=180
-    export function turnAsync(direction: Direction, angle: number) {
-        let b = board();
-
-        if (direction == Direction.Left)
-            b.robot.angle -= angle;
-        else
-            b.robot.angle += angle;
-        return Promise.delay(400)
+    //% blockId=moveForward block="walk forward"
+    export function moveForwardAsync() {
+        board().moveForward();
+        board().printRobot();
+        return Promise.delay(100)
     }
 
-    /**
-     * Triggers when the robot bumps a wall
-     * @param handler 
-     */
-    //% blockId=onBump block="on bump"
-    export function onBump(handler: RefAction) {
-        let b = board();
-
-        b.bus.listen("Robot", "Bump", handler);
-    }
 }
 
 
 
-namespace pxsim.loops {
-    /**
-     * Repeats the code forever in the background. On each iteration, allows other code to run.
-     * @param body the code to repeat
-     */
-    //% help=functions/forever weight=55 blockGap=8
-    //% blockId=device_forever block="forever" 
-    export function forever(body: RefAction): void {
-        thread.forever(body)
-    }
+// namespace pxsim.loops {
+//     /**
+//      * Repeats the code forever in the background. On each iteration, allows other code to run.
+//      * @param body the code to repeat
+//      */
+//     //% help=functions/forever weight=55 blockGap=8
+//     //% blockId=device_forever block="forever" 
+//     export function forever(body: RefAction): void {
+//         thread.forever(body)
+//     }
 
-    /**
-     * Pause for the specified time in milliseconds
-     * @param ms how long to pause for, eg: 100, 200, 500, 1000, 2000
-     */
-    //% help=functions/pause weight=54
-    //% block="pause (ms) %pause" blockId=device_pause
-    export function pauseAsync(ms: number) {
-        return Promise.delay(ms)
-    }
-}
+//     /**
+//      * Pause for the specified time in milliseconds
+//      * @param ms how long to pause for, eg: 100, 200, 500, 1000, 2000
+//      */
+//     //% help=functions/pause weight=54
+//     //% block="pause (ms) %pause" blockId=device_pause
+//     export function pauseAsync(ms: number) {
+//         return Promise.delay(ms)
+//     }
+// }
