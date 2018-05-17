999c19cc5042eb7daca0d321c12ce3ce51b39e5a
Aruhan Liu <aruhanliu2@gmail.com>
2018-05-17 11:27:50 -0400

    screenshot

--- projects/Untitled/main.ts
+++ projects/Untitled/main.ts
@@ -1,2 +1,10 @@
+Robot.faceUp()
+for (let i = 0; i < 5; i++) {
+    Robot.moveForward()
+}
+Robot.turnRight()
 Robot.moveForward()
-Robot.moveForward()
+Robot.turnLeft()
+for (let i = 0; i < 5; i++) {
+    Robot.moveForward()
+}
